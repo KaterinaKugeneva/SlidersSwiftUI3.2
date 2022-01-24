73897565cf109f55dc726e4cf76cd3982e14722d
KaterinaKugeneva <kugeneva1@gmail.com>
2022-01-24 10:40:00 +0300

    Initial Commit

---
+++ SlidersSwiftUI3.2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlidersSwiftUI3.2
+//
+//  Created by Ekaterina Kugeneva on 24.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlidersSwiftUI3.2/SlidersSwiftUI3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersSwiftUI3_2App.swift
+//  SlidersSwiftUI3.2
+//
+//  Created by Ekaterina Kugeneva on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SlidersSwiftUI3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
