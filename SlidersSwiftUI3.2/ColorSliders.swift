fb264b502e955d1423d88a5e4eb72bbfdc6885b4
KaterinaKugeneva <kugeneva1@gmail.com>
2022-01-24 16:37:39 +0300

    main functions added

---
+++ SlidersSwiftUI3.2/ColorSliders.swift
@@ -0,0 +1,27 @@
+//
+//  ColorSliders.swift
+//  SlidersSwiftUI3.2
+//
+//  Created by Ekaterina Kugeneva on 24.01.2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct ColorSliderView: View {
+    @Binding var value: Double
+    let sliderColor: Color
+    
+    var body: some View {
+        HStack {
+            Text("0")
+            Slider(value: $value, in: 0...255, step: 1)
+                .accentColor(sliderColor)
+            TextField("255", value: $value, formatter: NumberFormatter() )
+                .textFieldStyle(.roundedBorder)
+                .frame(width: 43)
+                .keyboardType(.decimalPad)
+            
+        }
+    }
+}
--- SlidersSwiftUI3.2/ContentView.swift
+++ SlidersSwiftUI3.2/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var sliderValueRed = Double.random(in: 0...255)
+    @State private var sliderValueGreen = Double.random(in: 0...255)
+    @State private var sliderValueBlue = Double.random(in: 0...255)
+    
+    @State private var userName = ""
+    @State private var displayedName = ""
+  
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 30) {
+            
+            Rectangle()
+                .frame(width: 380, height: 200)
+                .foregroundColor(Color(red: sliderValueRed/255, green: sliderValueGreen/255, blue: sliderValueBlue/255))
+                .overlay(Rectangle().stroke(Color.black, lineWidth: 2))
+            
+            ColorSliderView(value: $sliderValueRed, sliderColor: .red)
+            ColorSliderView(value: $sliderValueGreen, sliderColor: .green)
+            ColorSliderView(value: $sliderValueBlue, sliderColor: .blue)
+            
+            
+        }.padding()
+        
     }
 }
 
